2dd97670ffe394c89a37f66d6cacedd5c407f310
Felworl22 <135156503+Felworl22@users.noreply.github.com>
2023-12-11 05:50:47 +0000

    update file

---
+++ MultiMiner.Discovery.Tests/ListenerTests.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Sockets;
+
+namespace MultiMiner.Discovery.Tests
+{
+    [TestClass]
+    public class ListenerTests
+    {
+    }
+}
