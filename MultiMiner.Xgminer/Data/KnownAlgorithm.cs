5d0d05d15f34ec0e81c848b71996eed79dbf91a6
Felworl22 <135156503+Felworl22@users.noreply.github.com>
2023-12-11 05:53:34 +0000

    update file

---
+++ MultiMiner.Xgminer/Data/KnownAlgorithm.cs
@@ -0,0 +1,9 @@
+namespace MultiMiner.Xgminer.Data
+{
+    public class KnownAlgorithm
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public double Multiplier { get; set; }
+    }
+}
