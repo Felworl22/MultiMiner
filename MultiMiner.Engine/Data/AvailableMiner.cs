18e3e28397f65ed0970b104929b188bad41ac239
Felworl22 <135156503+Felworl22@users.noreply.github.com>
2023-12-11 05:52:05 +0000

    update file

---
+++ MultiMiner.Engine/Data/AvailableMiner.cs
@@ -0,0 +1,9 @@
+namespace MultiMiner.Engine.Data
+{
+    public class AvailableMiner
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Url { get; set; }
+    }
+}
