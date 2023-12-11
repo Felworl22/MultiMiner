b6649ea21ac255b9a9ca0c52197a111037054e0a
Felworl22 <135156503+Felworl22@users.noreply.github.com>
2023-12-11 05:52:23 +0000

    update file

---
+++ MultiMiner.TUI/Data/PoolListEntry.cs
@@ -0,0 +1,11 @@
+using MultiMiner.Engine.Data.Configuration;
+using MultiMiner.Xgminer.Data;
+
+namespace MultiMiner.TUI.Data
+{
+    struct PoolListEntry
+    {
+        public Coin Configuration;
+        public MiningPool Pool;
+    }
+}
